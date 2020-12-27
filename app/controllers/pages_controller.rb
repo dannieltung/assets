b8de2c60e6558f86929eacaac836dea12a7bf7a2
Danniel Tung <dwftung@gmail.com>
2020-12-27 14:21:28 -0300

    number to currency refactor

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,11 @@ class PagesController < ApplicationController
   def home
     assets = Asset.where(user: current_user)
     @assets = assets.sort_by { |event| [event.trade] }
+    @total_invested = 0
+    @assets.each do |asset|
+      if asset.operation == "C"
+        @total_invested += ((asset.quantity * asset.price) + asset.emoluments)
+      end
+    end
   end
 end
